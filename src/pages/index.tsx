023ec558dab03b93040f2963504c2d4aaaa53831
tuanl274 <tuanlq@meeyland.com>
2024-10-16 00:24:02 +0700

    fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,4 @@
-import localFont from 'next/font/local'
-
 import dynamic from 'next/dynamic'
 const Home = dynamic(() => import('@/components/Home'), { ssr: false })
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900'
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900'
-})
 
 export default Home
